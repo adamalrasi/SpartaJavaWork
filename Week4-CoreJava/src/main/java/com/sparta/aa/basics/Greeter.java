b0ad12c65bb00c972a63e27163238d64570897ba
Adam Alrasi <adam.alrasi1@gmail.com>
2024-03-12 13:36:17 +0000

    Refactored Greeter class and added Junit to Maven

---
+++ Week4-CoreJava/src/main/java/com/sparta/aa/basics/Greeter.java
@@ -0,0 +1,22 @@
+package com.sparta.aa.basics;
+/*
+1. Read
+2. Maintain
+3. Test
+ */
+
+public class Greeter {
+    public static void main(String[] args){ // entry point
+        int timeOfDay = 10;
+        System.out.println(getGreeting(timeOfDay));
+    }
+    private static String getGreeting(int timeOfDay) {
+        if (timeOfDay >= 5 && timeOfDay <= 12) {
+            return "Good morning!";
+        } else if (timeOfDay >= 12 && timeOfDay <= 18) {
+            return "Good afternoon!";
+        } else {
+            return "Good evening!";
+        }
+    }
+}
