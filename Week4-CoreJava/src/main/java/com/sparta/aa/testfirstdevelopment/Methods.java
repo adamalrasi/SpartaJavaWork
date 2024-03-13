02b68417d19a9891727c8f975358b9c7224d4f84
Adam Alrasi <adam.alrasi1@gmail.com>
2024-03-13 13:16:20 +0000

    Test-First-Development-Practices

---
+++ Week4-CoreJava/src/main/java/com/sparta/aa/testfirstdevelopment/Methods.java
@@ -0,0 +1,34 @@
+package com.sparta.aa.testfirstdevelopment;
+
+public class Methods {
+
+    // implement this method so that it returns true if x is greater than or equal to y
+    public static boolean greaterEqual(int x, int y) {
+        return x >= y;
+    }
+
+    // implement this method so that it take an int as an input,
+    // squares it, adds 101, divides the result by 7, then subtracts 4.
+    // return a double.
+    public static double BODMAS(int inputNumber) {
+        return 0;
+    }
+
+
+    // implement this method so that it returns true if input is "password"
+    // regardless of case
+    public static boolean checkInput(String input) {
+        return false;
+    }
+
+    // implement this method so that it returns the sum of all the numbers from start to end
+    public static int sumNums(int start, int end ) {
+        return -1;
+    }
+
+    // implement this method so that it returns the sum
+    // of all the numbers in the array nums
+    public static int sumArray(int[] nums) {
+        return -1;
+    }
+}
