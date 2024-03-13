26bdfd48b2996cceb484543b2c264af6d6d966ea
Adam Alrasi <adam.alrasi1@gmail.com>
2024-03-13 19:26:52 +0000

    Added some test cases about the CounterApp file

--- Week4-CoreJava/src/test/java/com/sparta/aa/staticfinallab/CounterTest.java
+++ Week4-CoreJava/src/test/java/com/sparta/aa/staticfinallab/CounterTest.java
@@ -3,6 +3,8 @@ package com.sparta.aa.staticfinallab;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class CounterTest {
 
@@ -26,10 +28,30 @@ public class CounterTest {
     @Test
     @DisplayName("Check if the increment methods works correctly, return count")
     public void checkIfIncrementIsWorking(){
+        // Arrange
         int expected = 11;
         Counter actual = new Counter("Test", 10);
+
+        // Act
         actual.increment();
+
+        // Assert
         Assertions.assertEquals(expected, actual.getCount());
+    }
+
+    @Test
+    @DisplayName("Check if Global Count is working correctly, return Global Count")
+    public void checkIfGlobalCountIsWorking(){
+        // Arrange
+        int expected = 10;
+        Counter test1 = new Counter("Test1", 0);
+        Counter test2 = new Counter("Test1", 0);
+
+        // Act
+        test1.setGlobalCount(10);
+
+        // Assert
+        Assertions.assertEquals(expected, test2.getGlobalCount());
     }
 
 }
