0db8089725efe4dc7851638eea9d98052aba3e44
Adam Alrasi <adam.alrasi1@gmail.com>
2024-03-14 11:01:16 +0000

    abstrract exercise

---
+++ Week4-CoreJava/src/main/java/com/sparta/aa/abstractclasses/App.java
@@ -0,0 +1,8 @@
+package com.sparta.aa.abstractclasses;
+
+public class App {
+    public static void main(String[] args) {
+        Customer customer = new Customer("Adam", "Alrasi");
+        customer.
+    }
+}
